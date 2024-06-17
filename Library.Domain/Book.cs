35896d4342ee82b4cc75173cbe1550ad80106bd0
Ilya Suprunovich <suprunovichilja@gmail.com>
2024-06-17 21:02:10 +0300

    Creating the application structure

---
+++ Library.Domain/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string ISBN { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime TimeOfTake { get; set; }
+        public DateTime TimeOfReturn { get; set; }
+
+    }
+}
